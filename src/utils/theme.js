eb98d7bd5eba092b75082f7226b38f27dc340e62
sir-olat <tunbosunogunlana1@gmail.com>
2019-09-13 03:52:23 +0100

    finish social-app v1

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -86,6 +86,51 @@
       width: '50%',
       backgroundColor: 'rgba(0,0,0,0.6)',
       marginBottom: 10
+    },
+    paper: {
+      padding: 20
+    },
+    profile: {
+      '& .image-wrapper': {
+        textAlign: 'center',
+        position: 'relative',
+        '& button': {
+          position: 'absolute',
+          top: '80%',
+          left: '70%'
+        }
+      },
+      '& .profile-image': {
+        width: 200,
+        height: 200,
+        objectFit: 'cover',
+        maxWidth: '100%',
+        borderRadius: '50%'
+      },
+      '& .profile-details': {
+        textAlign: 'center',
+        '& span, svg': {
+          verticalAlign: 'middle'
+        },
+        '& a': {
+          color: '#008394'
+        }
+      },
+      '& hr': {
+        border: 'none',
+        margin: '0 0 10px 0'
+      },
+      '& svg.button': {
+        '&:hover': {
+          cursor: 'pointer'
+        }
+      }
+    },
+    buttons: {
+      textAlign: 'center',
+      '& a': {
+        margin: '20px 10px'
+      }
     }
 
   }
